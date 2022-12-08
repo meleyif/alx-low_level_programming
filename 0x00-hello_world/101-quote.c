c6c2d678413e371796afe7547de49d715ab1b979
meleyif <meleyif@gmail.com>
2022-12-08 15:39:47 -0800

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio>
+/**
+ * main -Entry point
+ * Description: prints a string to tha standard error
+ * Return: Always 1 (Error)
+ */
+int main(void)
+{
+	char err[] = "add that piece of art is useful\" -Dora Korpar, 2015-10-199\n";
+	write(2, err, 59);
+		return (1);
