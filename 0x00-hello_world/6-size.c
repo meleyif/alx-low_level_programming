760af46e2ede73e536d7a00aa521fca4ad74f132
meleyif <meleyif@gmail.com>
2022-12-08 14:56:54 -0800

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
-
 /**
  * main - Entry point
- *
  * Return: Always 0 (Success)
  */
 int main(void)
@@ -12,6 +10,5 @@ int main(void)
 	printf("Size of long int: %zu byte(s)\n", sizeof(long int));
 	printf("Size of long long int: %zu byte(s)\n", sizeof(long long int));
 	Printf("Size of float: %zu byte(s)\n", sizeof(float));
-
 	return (0);
 }
