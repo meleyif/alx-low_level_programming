443739137760f46a34c0631e5f9c2bcdf4f372ee
meleyif <meleyif@gmail.com>
2022-12-08 16:01:02 -0800

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main -Entry point
- * Description: prints a string to tha standard error
- * Return: Always 1 (Error)
+ * main - Entry point
+ * Return: Always 1 
  */
+
 int main(void)
 {
-	write(2, "add that piece of art is useful\" -Dora Korpar, 2015-10-199\n", 58);
+	write(2, "add that piece of art is useful\" -Dora Korpar, 2015-10-199\n", 59);
 	return (1);
