33108d5bcf394e611cd84604e0f071112d44fb42
meleyif <meleyif@gmail.com>
2022-12-08 16:20:31 -0800

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,10 @@
 #include <unistd.h>
 /**
  * main - Entry point
- * Return: Always 1 
+ * Return: Always 1
  */
 int main(void)
 {
-	write(2, "add that piece of art is useful\" - Dora Korpar, 2015-10-199\n", 59);
+	write(2, "add that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
