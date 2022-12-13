7e5ac2899c2639d5815ff0e898fe3cc7295e21ac
meleyif <meleyif@gmail.com>
2022-12-13 14:23:33 -0800

    islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * _islower - checks for lowercase characters
- * @c: The character to be printed
- * Return: 1 in success & 0 if else
+ * _islower - checks if a character is lowercase
+ * @c: the character
+ * Return: 1 if letter is lowercase. 0 if not
  */
 int _islower(int c)
 {
