fcb76c72c13e52473404de08d297fe9bf96788dd
meleyif <meleyif@gmail.com>
2022-12-13 10:16:41 -0800

    alphabet1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - Entry point _putchar
+ * description: print_alphabet
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	print_alphabet n;
+	int n;
+
+	for (n = 'a'; n <= 'z'; n++)
+	{
+		_putchar(n);
+	}
+	_putchar('\n');
+	return (0);
+}
