4e09a2e4c9761fbf38a578f64a3c53bf767ea5f1
meleyif <meleyif@gmail.com>
2022-12-13 11:08:48 -0800

    alphabetx10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints out alphabet 10x
+ *
+ * Return: Always 0.
+ */
+int print_alphabet_x10(void)
+{
+	int a = 10
+		while (a < 10)
+		{
+			char c;
+
+			for (c = 'a'; c <= 'z'; c++)
+			{
+				_putchar(c);
+			}
+			_putchar('\n');
+		}
+}
