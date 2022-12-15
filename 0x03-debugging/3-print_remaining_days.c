9010575bf459478fa84b615869473b68be79bda8
meleyif <meleyif@gmail.com>
2022-12-15 14:23:39 -0800

    neumbers

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -10,16 +10,13 @@
  */
 void print_remaining_days(int month, int day, int year)
 {
-	int month;
-	int day;
-	int year;
-
-	if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0 && year % 100 == 0))
+	if ((year % 4 == 0) && (year % 400 == 0 || year % 100 != 0))
 	{
 		if (month >= 3 && day >= 60)
 		{
 			day++;
 		}
+
 		printf("Day of the year: %d\n", day);
 		printf("Remaining days: %d\n", 366 - day);
 	}
@@ -36,3 +33,4 @@ void print_remaining_days(int month, int day, int year)
 		}
 	}
 }
+
