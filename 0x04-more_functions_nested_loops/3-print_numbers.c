fc39dc32c9d9c119fa5c060aa8d07ee01c42931f
meleyif <meleyif@gmail.com>
2022-12-15 13:59:31 -0800

    numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+  * print_numbers - Print the numbers since 0 up to 9
+  *
+  * Return: The numbers since 0 up to 9
+  */
+void print_numbers(void)
+{
+	int a;
+
+	for (a = 0; a <= 9; a++)
+	{
+		_putchar(a + '0');
+	}
+
+	_putchar('\n');
+}
