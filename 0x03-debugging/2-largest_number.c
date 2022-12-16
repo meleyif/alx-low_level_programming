63ed8601b0a7281d7edc361da3e84728b8812c12
meleyif <meleyif@gmail.com>
2022-12-16 09:32:04 -0800

    largest

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,17 +12,23 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a > b && b > c)
+	if (a > b && a > c)
 	{
 		largest = a;
 	}
-	else if (b > a && b > c)
+	else if (b > c && b > a)
 	{
 		largest = b;
 	}
-	else
+	else if (c > b)
 	{
 		largest = c;
 	}
+	else
+	{
+		largest = b;
+	}
+
+
 	return (largest);
 }
