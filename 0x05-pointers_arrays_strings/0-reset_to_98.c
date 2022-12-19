c67c60598c418158e5fa297356fef03e547de0aa
meleyif <meleyif@gmail.com>
2022-12-19 09:54:55 -0800

    msg

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,2 +1,20 @@
-#include <tsdio>
+#include "main.h"
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void reset_to_98(int*n)
+{
+	int n;
+	int *p;
 
+	n = 402;
+	p = &n;
+
+	printf("n=%d\n", n);
+	*p = 98;
+	reset_to_98(&n);
+	printf("n=%d\n", n);
+	return 0;
+}
