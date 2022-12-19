7c5a4a4a27416e9dd3e4940dd8bb65bb456aa765
meleyif <meleyif@gmail.com>
2022-12-19 12:54:57 -0800

    msg

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,26 +8,5 @@
   */
 void rev_string(char *s)
 {
-	int i, c, k;
-	char *a, aux;
+	char *s;
 
-	a = s;
-
-	while (s[c] != '\0')
-	{
-		c++;
-	}
-
-	for (k = 1; k < c; k++)
-	{
-		a++;
-	}
-
-	for (i = 0; i < (c / 2); i++)
-	{
-		aux = s[i];
-		s[i] = *a;
-		*a = aux;
-		a--;
-	}
-}
