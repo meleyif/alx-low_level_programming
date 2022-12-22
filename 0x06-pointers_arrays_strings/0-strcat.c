a3c9b3e8d51739b51de06a59ac764581e777224e
meleyif <meleyif@gmail.com>
2022-12-22 09:44:36 -0800

    msg

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * *_strcat - concartenatew two strings
+ * @dest: The destination string
+ * @src: The source string
+ *
+ * Return: A pointer ro the resulting string dest.
+ */
+char *_strcat(char *dest, char *src)
+{
+	int dlen = 0, i;
+
+	while (dest[dlen])
+	{
+		dlen++;
+	}
+
+	for (i = 0; src[i] != 0; i++)
+	{
+		dest[dlen] = scr[i];
+		dlen++;
+	}
+	dest[dlen] = '\0';
+	return (dest);
+}
