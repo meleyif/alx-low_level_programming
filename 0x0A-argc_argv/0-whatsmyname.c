98d7b9d28b53f12e880ddc93692ed0ae5dcbc057
meleyif <meleyif@gmail.com>
2023-01-06 01:48:08 -0800

    msg

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Prints its name
+ * @argc: argumet count
+ * @argv: argument vector
+ */
+
+int main(int argc, char* argv[])
+{
+	(void) argc;
+	printf("%s", argv[0]);
+
+	return 0;
+}
