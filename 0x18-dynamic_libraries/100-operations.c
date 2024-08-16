c873e8ceab4341beb21f071ce0f29e0812a62d8f
root <meleyif@gmail.com>
2024-08-16 04:18:57 -0700

    dunamic library

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int add(int a, int b) {
+    return a + b;
+}
+
+int sub(int a, int b) {
+    return a - b;
+}
+
+int mul(int a, int b) {
+    return a * b;
+}
+
+int div(int a, int b) {
+    if (b != 0) {
+        return a / b;
+    } else {
+        return 0; // Handle division by zero
+    }
+}
+
+int mod(int a, int b) {
+    if (b != 0) {
+        return a % b;
+    } else {
+        return 0; // Handle modulus by zero
+    }
+}
